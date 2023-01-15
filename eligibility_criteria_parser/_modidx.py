f3eff6a94d9058a937bcfab592de9e264a1acfb2
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2023-01-15 23:19:10 +0100

    feat: add criteria extractor

--- eligibility_criteria_parser/_modidx.py
+++ eligibility_criteria_parser/_modidx.py
@@ -5,5 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://Mikołaj Morzy.github.io',
                 'git_url': 'https://github.com/Mikołaj Morzy/eligibility_criteria_parser',
                 'lib_path': 'eligibility_criteria_parser'},
-  'syms': { 'eligibility_criteria_parser.core': { 'eligibility_criteria_parser.core.foo': ( 'core.html#foo',
-                                                                                            'eligibility_criteria_parser/core.py')}}}
+  'syms': { 'eligibility_criteria_parser.core': { 'eligibility_criteria_parser.core.extract_entities': ( 'core.html#extract_entities',
+                                                                                                         'eligibility_criteria_parser/core.py'),
+                                                  'eligibility_criteria_parser.core.load_eligibility_criteria': ( 'core.html#load_eligibility_criteria',
+                                                                                                                  'eligibility_criteria_parser/core.py')}}}
