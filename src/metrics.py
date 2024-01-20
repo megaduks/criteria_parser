60ea903ce5fdc122eaf68f730c94fff0f542fc0c
megaduks <mikolaj.morzy@gmail.com>
2024-01-20 02:49:22 +0100

    feat: add Jaccard score metric

---
+++ src/metrics.py
@@ -0,0 +1,23 @@
+from typing import Set
+
+
+def jaccard_score(a: Set, b: Set, mode: str = "strict") -> float:
+    """Computes different versions of the Jaccard score depending on the requested mode
+
+    strict: |a & b| / |a + b|
+    relaxed: |a & b| / min{|a|,|b|}
+    left: |a & b| / |a|
+    right: |a & b| / |b|
+    """
+
+    if (not a) or (not b):
+        return 0.
+
+    if mode == "strict":
+        return len(a.intersection(b)) / len(a.union(b))
+    elif mode == "relaxed":
+        return len(a.intersection(b)) / min(len(a), len(b))
+    elif mode == "left":
+        return len(a.intersection(b)) / len(a)
+    elif mode == "right":
+        return len(a.intersection(b)) / len(b)
