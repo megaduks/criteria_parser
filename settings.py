c92fe23f941214c75bee9906ee2fef9abbe04a82
megaduks <mikolaj.morzy@gmail.com>
2024-01-22 22:02:07 +0100

    fix: correct path to FB dataset

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-PROJECT_DIR = Path(__file__).parent.parent
+PROJECT_DIR = Path(__file__).parent
 
 CHIA_PATH = PROJECT_DIR / "data/chia/"
-FB_PATH = PROJECT_DIR / "/fb/fb_dset_preprocessed.json"
+FB_PATH = PROJECT_DIR / "data/fb/fb_dset_preprocessed.json"
